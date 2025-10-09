1f14ec6c06b4f604cae77037a639b8235ae3b040
Régi GOUALE <regi.gouale@gmail.com>
2025-10-10 01:19:41 +0200

    feat: initialize NestJS application with basic structure and tests
    - Created AppController and AppService with a simple "Hello World!" endpoint.
    - Added AppModule to organize the application structure.
    - Implemented main entry point for the application.
    - Added unit tests for AppController.
    - Created end-to-end tests for the application.
    - Configured Jest for testing with a dedicated e2e configuration.
    - Set up TypeScript configuration for the project.

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,34 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  {
+    ignores: ['eslint.config.mjs'],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+    },
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+);
